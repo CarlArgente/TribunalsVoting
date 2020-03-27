ad584ded0fb9f952550b1b034c8f881e6c559932
CarlArgente <53144043+CarlArgente@users.noreply.github.com>
2020-03-27 18:10:05 +0800

    Admin Fixed NavigationBar

--- TribunalsVoting/AdminModule.xaml.cs
+++ TribunalsVoting/AdminModule.xaml.cs
@@ -22,14 +22,109 @@ namespace TribunalsVoting
         public AdminModule()
         {
             InitializeComponent();
+            Navigation.Visibility = Visibility.Collapsed;
             GridPrincipal.Children.Clear();
             GridPrincipal.Children.Add(new DashBoard());
         }
 
         private void ListViewItem_Selected(object sender, RoutedEventArgs e)
         {
+            //Event for Dashboard
+            ClickedHome();
+        }
+
+        private void ListViewItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            //Event for Candidates
+            ClickedCandidates();
+        }
+
+        private void ListViewItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+            //Event for Students
+            ClickedStudent();
+        }
+
+        private void ListViewItem_Selected_3(object sender, RoutedEventArgs e)
+        {
+            //Event for Admin
+            ClickedAdmin();
+        }
+
+        private void ListViewItem_Selected_4(object sender, RoutedEventArgs e)
+        {
+            //Event for Ranking
+            ClickedRanking();
+        }
+
+        private void ListViewItem_Selected_5(object sender, RoutedEventArgs e)
+        {
+            //Event for History
+            ClickedHistory();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Quit");
+        }
+
+        public void ClickedHome()
+        {
+            txtTitle.Text = "DashBoard";
+            Navigation.Visibility = Visibility.Collapsed;
             GridPrincipal.Children.Clear();
             GridPrincipal.Children.Add(new DashBoard());
+        }
+        public void ClickedCandidates()
+        {
+            txtTitle.Text = "Candidates";
+            Navigation.Visibility = Visibility.Visible;
+            dashText1.Text = "List of Candidate";
+            dashText2.Text = "Add Candidate";
+            dashText3.Text = "Reset Candidate";
+            dashDash1.Text = "|";
+            dashDash2.Text = "|";
+        }
+        public void ClickedStudent()
+        {
+            txtTitle.Text = "Students";
+            Navigation.Visibility = Visibility.Visible;
+            dashText1.Text = "List of Student";
+            dashText2.Text = "Add Student";
+            dashText3.Text = "";
+            dashDash1.Text = "|";
+            dashDash2.Text = " ";
+        }
+        public void ClickedAdmin()
+        {
+            txtTitle.Text = "Admin";
+            Navigation.Visibility = Visibility.Visible;
+            dashText1.Text = "Admin Accounts";
+            dashText2.Text = "Add Admin";
+            dashText3.Text = "";
+            dashDash1.Text = "|";
+            dashDash2.Text = "";
+
+        }
+        public void ClickedRanking()
+        {
+            txtTitle.Text = "Ranking";
+            Navigation.Visibility = Visibility.Visible;
+            dashText1.Text = "Winners";
+            dashText2.Text = "";
+            dashText3.Text = "";
+            dashDash1.Text = "";
+            dashDash2.Text = "";
+        }
+        public void ClickedHistory()
+        {
+            txtTitle.Text = "History";
+            Navigation.Visibility = Visibility.Visible;
+            dashText1.Text = "Logs";
+            dashText2.Text = "";
+            dashText3.Text = "";
+            dashDash1.Text = "";
+            dashDash2.Text = "";
         }
     }
 }
