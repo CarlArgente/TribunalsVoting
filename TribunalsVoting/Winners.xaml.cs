773dafa3b4265022bf720185c98f2a37f352daa8
CarlArgente <53144043+CarlArgente@users.noreply.github.com>
2020-04-14 22:28:42 +0800

    Fixed Winners Module

--- TribunalsVoting/Winners.xaml.cs
+++ TribunalsVoting/Winners.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,40 +23,84 @@ namespace TribunalsVoting
     /// </summary>
     public partial class Winners : UserControl
     {
-        //tanggalin mo nalang kung papalitan mo na ng value galing database
-        public class DataObject
+
+        //for updating table
+        private void updateTable()
         {
-            public String A { get; set; }
-            public String B { get; set; }
-            public String C { get; set; }
-            public String D { get; set; }
+            try
+            {
+                MySqlDataAdapter adapter = new MySqlDataAdapter("(SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position as " +
+                    "'Candidate_Position',tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON " +
+                    "tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes)" +
+                    " AND tbl_candidates.Candidate_Position='President' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION (SELECT tbl_candidates.Candidate_Name " +
+                    "as 'Candidate_Name', tbl_candidates.Candidate_Position as 'Candidate_Position',tbl_candidates.Candidate_Party as 'Candidate_Party'," +
+                    " tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID " +
+                    "WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND tbl_candidates.Candidate_Position='Vice President for Academic Affair' " +
+                    "ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION(SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position " +
+                    "as 'Candidate_Position',tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON " +
+                    "tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) " +
+                    "AND tbl_candidates.Candidate_Position='Vice President For Internal Affair' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) " +
+                    "UNION (SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position as 'Candidate_Position'," +
+                    " tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON " +
+                    "tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) " +
+                    "AND tbl_candidates.Candidate_Position='Vice President For External Affair' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION " +
+                    "(SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position as 'Candidate_Position', tbl_candidates.Candidate_Party " +
+                    "as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID " +
+                    "WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND tbl_candidates.Candidate_Position='Vice President For Operation' " +
+                    "ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION (SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position " +
+                    "as 'Candidate_Position',tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN " +
+                    "tbl_votes ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND" +
+                    " tbl_candidates.Candidate_Position='Vice President For Finance' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) " +
+                    "UNION (SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position as 'Candidate_Position', " +
+                    "tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes " +
+                    "ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND " +
+                    "tbl_candidates.Candidate_Position='ICT Representative' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION (SELECT tbl_candidates.Candidate_Name " +
+                    "as 'Candidate_Name', tbl_candidates.Candidate_Position as 'Candidate_Position', tbl_candidates.Candidate_Party as 'Candidate_Party', " +
+                    "tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE" +
+                    "(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND tbl_candidates.Candidate_Position='Engineering Representative' ORDER BY " +
+                    "tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION (SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position " +
+                    "as 'Candidate_Position',tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes " +
+                    "ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND " +
+                    "tbl_candidates.Candidate_Position='Accountancy, Business and Management Representative' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION " +
+                    "(SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position as 'Candidate_Position',tbl_candidates.Candidate_Party " +
+                    "as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID " +
+                    "WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) AND tbl_candidates.Candidate_Position='Tourism and Hospitality Representative' " +
+                    "ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) UNION (SELECT tbl_candidates.Candidate_Name as 'Candidate_Name', tbl_candidates.Candidate_Position " +
+                    "as 'Candidate_Position',tbl_candidates.Candidate_Party as 'Candidate_Party', tbl_votes.Number_Of_Votes FROM tbl_candidates INNER JOIN tbl_votes " +
+                    "ON tbl_candidates.Candidate_id = tbl_votes.Candidate_ID WHERE(SELECT MAX(tbl_votes.Number_Of_Votes) FROM tbl_votes) " +
+                    "AND tbl_candidates.Candidate_Position='Arts and Science Representative' ORDER BY tbl_votes.Number_Of_Votes DESC LIMIT 1) ", getter.conn);
+                    getter.conn.Open();
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    dataGrid1.ItemsSource = ds.Tables[0].DefaultView;
+                    getter.conn.Close();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        void SetSizeColumns()
+        {
+            getter.conn.Close();
+            dataGrid1.Columns[0].Width = 130;
+            dataGrid1.Columns[1].Width = 300;
+            dataGrid1.Columns[2].Width = 130;
+            dataGrid1.Columns[2].Width = 130;
         }
 
         public Winners()
         {
             InitializeComponent();
-            //tanggalin mo nalang kung papalitan mo na ng value galing database
-            var list = new ObservableCollection<DataObject>();
-
-            list.Add(new DataObject() { A = "Car Emerson L Argente", B = "Accountancy, Business and management representative", C = "KaISA", D = "1000" });
-            list.Add(new DataObject() { A = "Justine Andrei Tan", B = "ICT representative", C = "KaISA", D = "999" });
-            list.Add(new DataObject() { A = "Jerric Bersamin", B = "Tourism and Hospitality representative", C = "KaISA", D = "500" });
-            list.Add(new DataObject() { A = "Maverick Cejo", B = "Vice President for External Affair", C = "KaISA", D = "800" });
-            list.Add(new DataObject() { A = "Car Emerson L Argente", B = "Accountancy, Business and management representative", C = "KaISA", D = "1000" });
-            list.Add(new DataObject() { A = "Justine Andrei Tan", B = "ICT representative", C = "KaISA", D = "999" });
-            list.Add(new DataObject() { A = "Jerric Bersamin", B = "Tourism and Hospitality representative", C = "KaISA", D = "500" });
-            list.Add(new DataObject() { A = "Maverick Cejo", B = "Vice President for External Affair", C = "KaISA", D = "800" });
-            list.Add(new DataObject() { A = "Car Emerson L Argente", B = "Accountancy, Business and management representative", C = "KaISA", D = "1000" });
-            list.Add(new DataObject() { A = "Justine Andrei Tan", B = "ICT representative", C = "KaISA", D = "999" });
-            list.Add(new DataObject() { A = "Jerric Bersamin", B = "Tourism and Hospitality representative", C = "KaISA", D = "500" });
-            list.Add(new DataObject() { A = "Maverick Cejo", B = "Vice President for External Affair", C = "KaISA", D = "800" });
-
-
-
-            this.dataGrid1.ItemsSource = list;
+            updateTable();
         }
 
         private void DataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid1_Loaded(object sender, RoutedEventArgs e)
         {
 
         }
